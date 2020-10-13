3f99b4df259fa1630833d0741bf214291e14a31a
KirillLvov <39922763+KirillLvov@users.noreply.github.com>
2020-10-13 23:01:19 +0300

    Update process.h

--- process.h
+++ process.h
@@ -8,7 +8,7 @@ public:
 	explicit Process(const std::string& path);
 	~Process();
 
-	size_t wwrite(const void* data, size_t len);
+	size_t write(const void* data, size_t len);
 	void writeExact(const void* data, size_t len);
 	size_t read(void* data, size_t len);
 	void readExact(void* data, size_t len);
